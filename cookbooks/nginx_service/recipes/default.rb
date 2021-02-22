199f605df7705bb45c3954baad59ef70cb9185ae
Salar Hafezi <salar@firstsource.tech>
2021-02-22 17:45:09 +0330

    Use templates for nginx_service

--- cookbooks/nginx_service/recipes/default.rb
+++ cookbooks/nginx_service/recipes/default.rb
@@ -5,6 +5,14 @@ package 'nginx' do
   action :install
 end
 
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
+
+template '/etc/nginx/sites-enabled/main.conf' do
+  source 'main.conf.erb'
+end
+
 service 'nginx' do
   action [:enable, :start]
 end
